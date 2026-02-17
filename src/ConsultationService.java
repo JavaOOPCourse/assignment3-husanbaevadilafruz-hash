94e5526b9022ebc76bc4750b2a2ff683df4ff6cc
Dilia <husanbaevadilafruz@gmail.com>
2026-02-17 21:43:51 +0600

    Update ConsultationService.java

--- src/ConsultationService.java
+++ src/ConsultationService.java
@@ -1,23 +1,20 @@
-public class ConsultationService extends Service
-        implements Billable {
+public class ConsultationService extends Service implements Billable {
 
-    // TODO: constructor
-
+    public ConsultationService(String name, int id) {
+        super(name, id);
+    }
 
     @Override
     public void performService() {
-
-        // TODO:
-        // check active
-        // print consultation message
-
+        if (!active) {
+            System.out.println(serviceName + " is inactive. Consultation unavailable.");
+            return;
+        }
+        System.out.println("Conducting online consultation...");
     }
 
     @Override
     public void generateBill() {
-
-        // TODO:
-        // print billing message
-
+        System.out.println("Generating bill for consultation session.");
     }
 }
