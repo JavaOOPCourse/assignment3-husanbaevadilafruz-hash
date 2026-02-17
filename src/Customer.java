7c6460ba4d01310e88053ade96071045103ebd5a
Dilia <husanbaevadilafruz@gmail.com>
2026-02-17 21:45:24 +0600

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,22 +1,21 @@
 public class Customer extends User {
 
-    // TODO: constructor
-
+    public Customer(String name, int id) {
+        super(name, id);
+    }
 
     @Override
     public void manageService(Service s) {
-
-        // TODO:
-        // print that customer cannot manage services
-
+        System.out.println("Customer cannot manage services.");
     }
 
     @Override
     public void useService(Service s) {
-
-        // TODO:
-        // print customer using message
-        // call performService()
-
+        if (s.isActive()) {
+            System.out.println(name + " is using " + s.getServiceName());
+            s.performService();
+        } else {
+            System.out.println(s.getServiceName() + " is not available.");
+        }
     }
 }
