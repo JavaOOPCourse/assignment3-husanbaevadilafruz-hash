3e716bd97c9eef6f77d1c7089cf401017d85b215
Dilia <husanbaevadilafruz@gmail.com>
2026-02-17 21:43:21 +0600

    Update CloudStorageService.java

--- src/CloudStorageService.java
+++ src/CloudStorageService.java
@@ -1,35 +1,29 @@
-public class CloudStorageService extends Service
-        implements PremiumFeature, Billable {
+public class CloudStorageService extends Service implements PremiumFeature, Billable {
 
-    // TODO: declare premium field
+    private boolean premium;
 
-
-    // TODO: constructor
-
+    public CloudStorageService(String name, int id) {
+        super(name, id);
+        this.premium = false;
+    }
 
     @Override
     public void performService() {
-
-        // TODO:
-        // check if active
-        // print cloud access message
-
+        if (!active) {
+            System.out.println(serviceName + " is inactive. Cannot access storage.");
+            return;
+        }
+        System.out.println("Accessing cloud storage...");
     }
 
     @Override
     public void upgradeToPremium() {
-
-        // TODO:
-        // enable premium
-        // print message
-
+        premium = true;
+        System.out.println(serviceName + " upgraded to Premium (extra storage).");
     }
 
     @Override
     public void generateBill() {
-
-        // TODO:
-        // print billing message
-
+        System.out.println("Generating bill for cloud storage usage.");
     }
 }
